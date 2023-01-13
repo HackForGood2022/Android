9b7c903a5e8573ab2cd8f96b8f657a9addda2885
Jaime Roncal <55926526+jaimeroncal@users.noreply.github.com>
2023-01-13 19:41:10 +0100

    Hardcoded pueblos (Servidor caido)

--- RuralApp/app/src/main/java/es/hackforgood/ruralapp/async/GetPueblosAsyncTask.java
+++ RuralApp/app/src/main/java/es/hackforgood/ruralapp/async/GetPueblosAsyncTask.java
@@ -17,6 +17,16 @@ import es.hackforgood.ruralapp.MapaActivity;
 public class GetPueblosAsyncTask extends AsyncTask<String, Void, GetPueblosAsyncTask.Result> {
     private MapaActivity mActivity = null;
 
+    private String jsonHard = "{" +
+            "result: \"success\"," +
+            "pueblos: [" +
+            "{ name: \"Villanueva de Sigena\", latitud: \"41.715278\", longitud: \"-0.008889\", cantidad: \"2\" },\n" +
+            "{ name: \"Alcolea\", latitud: \"36.966667\", longitud: \"-2.95\", cantidad: \"1\" },\n" +
+            "{ name: \"La Puebla de Hijar\", latitud: \"41.213239\", longitud: \"-0.445625\", cantidad: \"2\" },\n" +
+            "{ name: \"Fago\", latitud: \"42.735278\", longitud: \"-0.865556\", cantidad: \"1\" },\n" +
+            "{ name: \"Canfranc\", latitud: \"42.716656\", longitud: \"-0.525198\", cantidad: \"5\" }\n" +
+            "]}";
+
     public static class Pueblo {
         public String name;
         public String latitud;
@@ -34,27 +44,29 @@ public class GetPueblosAsyncTask extends AsyncTask<String, Void, GetPueblosAsync
     }
 
     protected GetPueblosAsyncTask.Result doInBackground(String... params) {
-        HttpURLConnection con;
+        //HttpURLConnection con;
         try {
+            /*
             con = (HttpURLConnection) new URL("https://ruralapp-backend.herokuapp.com/api/general/get_pueblos").openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("Content-Type", "application/json");
             con.setRequestProperty("Accept", "application/json");
-            con.setDoOutput(true);
+            con.setDoOutput(true);*/
 
+            /*
             String jsonInputString = "{}";
             try(OutputStream os = con.getOutputStream()) {
                 byte[] input = jsonInputString.getBytes();
                 os.write(input, 0, input.length);
-            }
+            }*/
 
-            InputStreamReader reader = new InputStreamReader(con.getInputStream());
+            //InputStreamReader reader = new InputStreamReader(con.getInputStream());
             Gson gson = new Gson();
-            return gson.fromJson(reader, GetPueblosAsyncTask.Result.class);
+            return gson.fromJson(jsonHard, GetPueblosAsyncTask.Result.class);
 
-        } catch (IOException e) {
+        } /*catch (IOException e) {
             Log.e("ERROR_GET_PUEBLOS",e.getMessage());
-        } catch (Exception e) {
+        }*/ catch (Exception e) {
             Log.e("ERROR_GET_PUEBLOS",e.getMessage());
         }
         return new Result();
